ad093c915a8d84dae2b78d8835ee92705d05c582
Kamollkin <kamola.nimatulaeva@gmail.com>
2025-08-12 17:52:54 +0600

    Update models.py

--- user/models.py
+++ user/models.py
@@ -15,7 +15,7 @@ class CustomUserManager(BaseUserManager):
         return user
 
     def create_superuser(self, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_staff', False)
         extra_fields.setdefault('is_superuser', True)
         extra_fields.setdefault('role', 'admin')
 
@@ -27,13 +27,13 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
         ('user', 'User'),
         ('admin', 'Admin'),
     )
-    id = models.AutoField
+
     email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=30, blank=True)
     last_name = models.CharField(max_length=30, blank=True)
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='user')
     is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
 
     
     objects = CustomUserManager()
